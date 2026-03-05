96d3d86ce4282e6151fb498ec203e41a8f77dfe3
KrX3D <KrX3D@users.noreply.github.com>
2026-03-06 00:43:42 +0100

    Update settings.js

--- mods/ui/settings.js
+++ mods/ui/settings.js
@@ -500,7 +500,7 @@ export default function modernUI(update, parameters) {
                                 title: 'Watched Videos Threshold',
                                 subtitle: 'Set the percentage threshold for hiding watched videos'
                             },
-                            options: [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100].map((percent) => {
+                            options: [0, 5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100].map((percent) => {
                                 return {
                                     name: `${percent}%`,
                                     key: 'hideWatchedVideosThreshold',
